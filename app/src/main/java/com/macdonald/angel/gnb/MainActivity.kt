bb994fa31392590cbd46616caee42761e64d6e14
amacdong <amacdong@everis.com>
2020-02-17 15:54:19 +0100

    Added nav host fragment and toolbar application

--- app/src/main/java/com/macdonald/angel/gnb/MainActivity.kt
+++ app/src/main/java/com/macdonald/angel/gnb/MainActivity.kt
@@ -2,11 +2,40 @@ package com.macdonald.angel.gnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbarApplication)
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment? ?: return
+
+        // Set up Action Bar
+        val navController = host.navController
+
+        val topLevelDestinations = hashSetOf<Int>() //Add the main destinations
+        //topLevelDestinations.add(R.id.fragment)
+
+        appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations)
+            .build()
+
+        setupActionBar(navController, appBarConfiguration)
+    }
+
+    private fun setupActionBar(navController: NavController,
+                               appBarConfig : AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfig)
     }
 }
