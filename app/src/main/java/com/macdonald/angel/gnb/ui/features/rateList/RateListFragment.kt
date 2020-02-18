16088147f34f83ee2a224f5e4927e3e8d2aedc34
amacdong <amacdong@everis.com>
2020-02-18 16:06:03 +0100

    Linked rates data to its adapter

--- app/src/main/java/com/macdonald/angel/gnb/ui/features/rateList/RateListFragment.kt
+++ app/src/main/java/com/macdonald/angel/gnb/ui/features/rateList/RateListFragment.kt
@@ -2,16 +2,18 @@ package com.macdonald.angel.gnb.ui.features.rateList
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.macdonald.angel.data.model.RateModel
-import com.macdonald.angel.data.model.TransactionModel
-
 import com.macdonald.angel.gnb.R
-import com.macdonald.angel.gnb.ui.features.transactionList.adapters.TransactionsListAdapter
+import com.macdonald.angel.gnb.ui.features.rateList.adapters.RatesListAdapter
+import es.dmoral.toasty.Toasty
+import kotlinx.android.synthetic.main.fragment_rate_list.*
 import org.koin.androidx.scope.currentScope
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -21,6 +23,9 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class RateListFragment : Fragment(), RateListContract.View {
 
     private val viewModel: RateListViewModel by currentScope.viewModel(this)
+
+    private lateinit var adapter: RatesListAdapter
+    private lateinit var listRates: ArrayList<RateModel>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,13 +40,62 @@ class RateListFragment : Fragment(), RateListContract.View {
         viewModel.model.observe(this, Observer(::updateUi))
     }
 
-
-    override fun initializeViews() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializeViews()
+        viewModel.getAllRates()
     }
 
-    override fun updateUi(model: RateListViewModel.UiModel) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+    override fun initializeViews() {
+        rvRates.layoutManager = LinearLayoutManager(context!!)
+    }
+
+    override fun updateUi(model: RateListViewModel.UiModel) = when(model) {
+        is RateListViewModel.UiModel.ShowRates -> {
+            listRates = model.rateList as ArrayList<RateModel>
+            adapter = RatesListAdapter(
+                context!!,
+                listRates
+            ) { rateItem: RateModel, _: View ->
+
+                val toast = Toasty.info(
+                    context!!,
+                    "Item clicked",
+                    Toast.LENGTH_LONG,
+                    true
+                )
+                toast.show()
+            }
+            rvRates.adapter = adapter
+        }
+        RateListViewModel.UiModel.Forbbiden -> {
+            val toast = Toasty.info(
+                context!!,
+                getString(R.string.not_get_rates),
+                Toast.LENGTH_LONG,
+                true
+            )
+            toast.show()
+        }
+        RateListViewModel.UiModel.ErrorGettingRates -> {
+            val toast = Toasty.info(
+                context!!,
+                getString(R.string.not_get_rates),
+                Toast.LENGTH_LONG,
+                true
+            )
+            toast.show()
+        }
+        RateListViewModel.UiModel.NetWorkError -> {
+            val toast = Toasty.info(
+                context!!,
+                getString(R.string.not_get_rates),
+                Toast.LENGTH_LONG,
+                true
+            )
+            toast.show()
+        }
     }
 
 }
