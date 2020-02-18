95cf330e890bdbbdac19665cc635da95e5b20182
amacdong <amacdong@everis.com>
2020-02-18 15:37:49 +0100

    Created the implementation of remote rates datasource

--- app/src/main/java/com/macdonald/angel/gnb/data/networking/datasources/rates/ConcretionRatesRemoteDatasource.kt
+++ app/src/main/java/com/macdonald/angel/gnb/data/networking/datasources/rates/ConcretionRatesRemoteDatasource.kt
@@ -4,10 +4,47 @@ import android.content.Context
 import com.macdonald.angel.data.repositories.Response
 import com.macdonald.angel.data.sources.RatesRemoteDatasource
 import com.macdonald.angel.domain.ratesUseCase.RateDomain
+import com.macdonald.angel.gnb.data.networking.NetworkObject
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import retrofit2.HttpException
+import kotlin.coroutines.resume
 
 class ConcretionRatesRemoteDatasource(val context: Context): RatesRemoteDatasource {
-    override suspend fun getRatesFromRemote(): Response<Array<RateDomain>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    private lateinit var rates: Response<Array<RateDomain>>
+
+    override suspend fun getRatesFromRemote(): Response<Array<RateDomain>> =
+        suspendCancellableCoroutine { continuation ->
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    val response = NetworkObject.service.getRates()
+
+                    if (response != null) {
+                        rates = Response.Success(response)
+                        continuation.resume(rates)
+                    } else {
+                        continuation.resume(Response.Error(Throwable()))
+                    }
+                } catch (e: Exception) {
+                    rates = when (e) {
+                        is HttpException -> {
+                            val isForbbiden = e.code() == 403 || e.code() == 401
+                            if (isForbbiden) {
+                                Response.Forbidden
+                            } else {
+                                Response.NetWorkError
+                            }
+                        }
+                        else -> {
+                            Response.NetWorkError
+                        }
+                    }
+                    continuation.resume(rates)
+                }
+            }
+        }
+
 }
 
